a6316b48715027fa6505ee9a0d87f5af18e6c680
D <1nymous1@gmail.com>
2020-06-10 21:38:44 -0500

    new

--- scripts/main.js
+++ scripts/main.js
@@ -60,6 +60,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("btn");
+  var caps = document.getElementsByClassName("caption_inc");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -68,8 +69,12 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  for (i = 0; i < caps.length; i++) {
+      caps[i].className = caps[i].className.replace(" active_cap", "");
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+  caps[slideIndex-1].className += " active_cap";
 } 
 
 
