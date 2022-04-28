eb11627d96681652837cd2671cd1949a538c1445
arbitraryurl <65641358+arbitraryurl@users.noreply.github.com>
2022-04-27 19:23:51 -0500

    2022 update

--- scripts/main.js
+++ scripts/main.js
@@ -1,193 +1,167 @@
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-  // coll[i].addEventListener("click", function() {
-    // this.classList.toggle("activeMain");
-    // var content = this.nextElementSibling;
-	// console.log(content.style.maxHeight);
-    // if (content.style.maxHeight){
-      // content.style.maxHeight = null;
-    // } else {
-      // content.style.maxHeight = content.scrollHeight + "px";
-    // }
-  // });
-// }
-
-if ( document.URL.includes("instant.html") ) {
-	document.querySelector('#here').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#first').classList.toggle('open');
-	});
-
-	document.querySelector('#here2').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#second').classList.toggle('open');
-	});
-
-	document.querySelector('#here5').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#fifth').classList.toggle('open');
-	});
-
-  document.querySelector('#here6').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#sixth').classList.toggle('open');
-  });
-  document.querySelector('#here7').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#seventh').classList.toggle('open');
-  });
-  document.querySelector('#here8').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#eigth').classList.toggle('open');
-  });
-  document.querySelector('#here9').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#ninth').classList.toggle('open');
-  });
-  document.querySelector('#here10').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#tenth').classList.toggle('open');
-  });
-  document.querySelector('#here11').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#eleventh').classList.toggle('open');
-  });
-
-
+//on index page
+if (window.location.pathname === '/' || document.URL.includes("index.html")){
+	console.log("INDEX")
+	const myButtons = document.querySelectorAll("#here");
+	for (const btn of myButtons) {
+			btn.addEventListener('click', function() {
+				this.classList.toggle("activeIdx");
+				const to_open = btn.getAttribute("data-for");
+				document.querySelector("#" + to_open).classList.toggle('open');
+			})
+		};
 }
 
-else {
-	document.querySelector('#here').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#first').classList.toggle('open');
-	});
-
-	document.querySelector('#here2').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#second').classList.toggle('open');
-	});
-
-	document.querySelector('#here3').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#third').classList.toggle('open');
-	});
-
-	document.querySelector('#here4').addEventListener('click', function() {
-		this.classList.toggle("activeIdx")
-		document.querySelector('#fourth').classList.toggle('open');
-	});
-  document.querySelector('#here5').addEventListener('click', function() {
-    this.classList.toggle("activeIdx")
-    document.querySelector('#fifth').classList.toggle('open');
-  });
+//On main page
+else{
+	console.log("MAIN")
+	// var datas = ["1", "2", "3"];
+	// localStorage["datas"] = JSON.stringify(datas);
+	//
+	// // Retrieve
+	// var stored_datas = JSON.parse(localStorage["datas"]);
+	// console.log(stored_datas);
+	const myButtons = document.querySelectorAll("#here_instant");
+	for (const btn of myButtons) {
+			btn.addEventListener('click', function() {
+				this.classList.toggle("activeIdx");
+				const to_open = btn.getAttribute("data-for");
+				document.querySelector("#" + to_open).classList.toggle('open');
+			});
+		};
 }
 
-var first = [1,2,3]
-var second = [4,5,6,7,8,9]
-var third = [10,11,12,13,14,15,16,17,18]
-var fourth = [19,20,21]
-var fifth = [22,23]
 
+
+//store slide number to present on new page
 function store(p) {
 	localStorage.setItem("imnum", p);
 }
 
+//store div id to open on new page
+function txt_store(t) {
+	localStorage.setItem("nextopen", t);
+	console.log(t)
+}
+
+// Retrieve
+var stored_datas = JSON.parse(localStorage["arr"]);
+console.log(stored_datas);
+
+//if we have stored divs and we're not on the index page
+if (stored_datas && !(window.location.pathname === '/' || document.URL.includes("index.html")) ){
+	var arr = stored_datas;
+}
+else{
+	var arr = [];
+}
+
+//append div to be opened to array
+function carryOn(d){
+	if (!arr.includes(d)){
+		arr.push(d);
+	}
+
+	else if (arr.includes(d)){
+		const arr_ind = arr.indexOf(d);
+		console.log(arr_ind);
+		arr.splice(arr_ind, 1);
+	}
+
+	console.log(arr);
+}
+
+//save array to local memory
+function setSwitch(){
+	localStorage["arr"] = JSON.stringify(arr);
+}
+
+
+
+
+//fetching local memory
+var thatopen = localStorage.getItem("nextopen");
 var slideIndex = parseInt(localStorage.getItem("imnum"));
 
 if (slideIndex){
-	showSlides(slideIndex);
-	console.log(slideIndex);
-} else {
-	slideIndex = 0;
-	showSlides(slideIndex);
-  console.log(slideIndex);
-}
-
-const first_logic = first.includes(parseInt(slideIndex));
-const second_logic = second.includes(parseInt(slideIndex));
-const third_logic = third.includes(parseInt(slideIndex));
-const fourth_logic = fourth.includes(parseInt(slideIndex));
-const fifth_logic = fifth.includes(parseInt(slideIndex));
-
-console.log(second_logic)
-
-
-if (first_logic){
-  document.querySelector('#here6').classList.toggle("activeIdx")
-  document.querySelector('#sixth').classList.toggle('open');
-}
-else if (second_logic){
-  document.querySelector('#here2').classList.toggle("activeIdx")
-  document.querySelector('#second').classList.toggle('open');  
-}
-else if (third_logic){
-  document.querySelector('#here').classList.toggle("activeIdx")
-  document.querySelector('#first').classList.toggle('open');  
-}
-else if (fourth_logic){
-  document.querySelector('#here10').classList.toggle("activeIdx")
-  document.querySelector('#tenth').classList.toggle('open');  
-}
-
-else if (fifth_logic){
-  document.querySelector('#here11').classList.toggle("activeIdx")
-  document.querySelector('#eleventh').classList.toggle('open');  
-}
-
-
-
-// Next/previous controls
-function plusSlides(n) {
-  slideIndex = slideIndex - n;
-  showSlides(slideIndex);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("btn");
-  var caps = document.getElementsByClassName("caption_inc");
-  // console.log(slideIndex);
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  for (i = 0; i < caps.length; i++) {
-      caps[i].className = caps[i].className.replace(" active_cap", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-
-  caps[slideIndex-1].className += " active_cap";
+	showArrow(slideIndex);
 
 }
 
+// //if a slide number is stored, show it; else set slide to 0
+// if (slideIndex){
+// 	showSlides(slideIndex);
+// } else {
+// 	slideIndex = 1;
+// 	thatopen = "instant1"
+// 	showSlides(slideIndex);
+// }
 
 
 
+//open div on new page (instant) using local memory
+const myButtons = document.querySelectorAll("#here_instant");
+console.log("HERE");
+console.log(myButtons);
+var arrayLength = stored_datas.length;
+for (const btn of myButtons) {
+	for (var i = 0; i<arrayLength; i++){
+		divToOpen = stored_datas[i];
+		console.log(divToOpen);
+		if (btn.getAttribute("data-for") == divToOpen){
+			btn.classList.toggle("activeIdx");
+			document.querySelector('#' + divToOpen).classList.toggle('open');
+		}
+	}
+}
 
-// // Get the container element
-// var btnContainer = document.getElementById("sidebuttons");
+if (thatopen == "about"){
+	document.querySelector("#about").classList.toggle("activeIdx");
+}
 
-// // Get all buttons with class="btn" inside the container
-// var btns = btnContainer.getElementsByClassName("btn");
+// if (thatopen == "contact"){
+// 	document.querySelector("#contact").classList.toggle("activeIdx");
+// }
 
-// // Loop through the buttons and add the active class to the current/clicked button
-// for (var i = 0; i < btns.length; i++) {
-  // btns[i].addEventListener("click", function() {
-    // var current = document.getElementsByClassName("active");
-    // current[0].className = current[0].className.replace(" active", "");
-    // this.className += " active";
-  // });
+function showArrow(n){
+	var dots = document.getElementsByClassName("btn");
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	dots[slideIndex-1].className += " active";
+}
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   slideIndex = slideIndex - n;
+//   showSlides(slideIndex);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("btn");
+//   var caps = document.getElementsByClassName("caption_inc");
+//   // console.log(slideIndex);
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   for (i = 0; i < caps.length; i++) {
+//       caps[i].className = caps[i].className.replace(" active_cap", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// 	if (slideIndex < caps.length){
+//   caps[slideIndex-1].className += " active_cap";
+// 	}
+//
 // }
